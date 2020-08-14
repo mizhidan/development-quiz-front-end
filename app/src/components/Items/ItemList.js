1a843a3ec193edc52e357ac50a671d4ca99b1c58
mizhidan <dj9824@126.com>
2020-08-14 17:50:47 +0800

    [danjie] feat: fetch data

--- app/src/components/Items/ItemList.js
+++ app/src/components/Items/ItemList.js
@@ -3,30 +3,37 @@ import Item from './Item';
 
 const url = "http://localhost:8080/items"
 
-class ItemList extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            itemList:{},
-        };
-    }
+class ItemList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemList: [],
+    };
+  }
 
-    getItems()
-    {
-        fetch(url).then(result => {
-            return result.json();
-        }).catch(error => {
-            console.log(error);
-        }).then(json => {
-            this.setState({itemList: json});
-        })
-    }
+  componentDidMount() {
+    fetch(url).then(result => {
+      return result.json();
+    }).catch(error => {
+      console.log(error);
+    }).then(json => {
+      this.setState({itemList: json});
+    })
+  }
 
-    render() {
+  render() {
+    console.log(this.state.itemList);
+    return (
+      this.state.itemList.map((key, index) => {
         return (
-            <Item />
-        )
-    }
+          <Item name={this.state.itemList[index].name}
+                pics={this.state.itemList[index].picUrl}
+                price={this.state.itemList[index].price}
+          />)
+
+      })
+    )
+  }
 }
 
 export default ItemList;
