6fb294e79d55e1ce5f2c7811fe143acc68f87c37
mizhidan <dj9824@126.com>
2020-08-18 20:43:57 +0800

    [danjie] refactor: use ant design header replace header

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import './Header.css';
+import { HomeOutlined,ShoppingCartOutlined,PlusOutlined } from '@ant-design/icons';
+import {Menu} from "antd";
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          theme: 'dark',
+          current: 'mall'
+        };
     }
 
-    render() {
-        return (
-            <BrowserRouter>
-                <div className="header">
-                <ul className="nav-header">
-                    <li><NavLink className="nav-link" exact to="/">商城</NavLink></li>
-                    <li><NavLink className="nav-link" exact to="/order">订单</NavLink></li>
-                    <li><NavLink className="nav-link" exact to="/additem">添加商品</NavLink></li>
-                </ul>
-                </div>
-            </BrowserRouter>
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
 
+    render() {
+      const { current } = this.state;
+        return (
+          <BrowserRouter>
+            <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal" theme={this.state.theme}>
+              <Menu.Item key="mall" icon={<HomeOutlined />} onClick={this.handleClick}>
+                <NavLink className="nav-link" exact to="/">商城</NavLink>
+              </Menu.Item>
+              <Menu.Item key="orders" icon={<ShoppingCartOutlined />} onClick={this.handleClick}>
+                <NavLink className="nav-link" exact to="/order">订单</NavLink>
+              </Menu.Item>
+              <Menu.Item key="add-item" icon={<PlusOutlined />} onClick={this.handleClick}>
+                <NavLink className="nav-link" exact to="/additem">添加商品</NavLink>
+              </Menu.Item>
+            </Menu>
+            </BrowserRouter>
         )
     }
 }
