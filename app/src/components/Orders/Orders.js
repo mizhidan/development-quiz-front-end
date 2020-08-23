b8b91522bae5a13333b7da9d8c8640afb1283823
mizhidan <dj9824@126.com>
2020-08-23 21:19:48 +0800

    [danjie] feat: delete order

--- app/src/components/Orders/Orders.js
+++ app/src/components/Orders/Orders.js
@@ -29,6 +29,39 @@ class Orders extends React.Component {
       })
   }
 
+  handleClick(id) {
+    const option = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    };
+    fetch(url+"/"+id, option)
+      .then(response => response.json())
+      .catch(error => console.log(error))
+      .then(() => this.deleteRow(id))
+  }
+
+  deleteRow(id) {
+    this.state.dataSource.forEach((item, index) => {
+      if (item.id === id) {
+        delete this.state.dataSource[index];
+      }
+    })
+    fetch(url)
+      .then(result => {
+        return result.json()
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .then(json => {
+        this.setState({
+          dataSource: json
+        })
+        console.log(this.state.dataSource)
+      })
+  }
 
   render() {
     const columns = [
@@ -55,16 +88,17 @@ class Orders extends React.Component {
       {
         title: '操作',
         key: 'action',
-        render: (text) => (
-          <Button type="default"><span>删除</span></Button>
+        render: (record) => (
+          <Button type="default " danger onClick={() => this.handleClick(record.id)}><span>删除</span></Button>
         )
       }
     ]
     return (
-      <div>
+      <div className="table">
         <Table
           dataSource={this.state.dataSource}
           columns={columns}
+          pagination={false}
           locale={
             {emptyText: '暂无订单，返回商城页面继续购买'}
           }
